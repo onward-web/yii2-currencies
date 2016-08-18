8736fb7c2cc9a75945c7a6df05d92f27f9c2bf9a
Vladimir Stovbchatij <unreality90@mail.ru>
2016-08-18 12:58:23 +0300

    Update

--- Console/CronController.php
+++ Console/CronController.php
@@ -18,15 +18,19 @@ class CronController extends Controller {
      */
     public function actionUpdate() {
         $rates = $this->getRates();
-        $this->stdout("Started update\n", Console::FG_BLUE);
-        foreach ($rates AS $key => $rate) {
-            $rateItem = Currency::findOne(['code' => $key]);
-            if ($rateItem) {
-                $rateItem->scenario = 'update';
-                $rateItem->rate = $rate;
-                $rateItem->save();
+        $this->stdout("Started update\n", Console::FG_GREEN);
+        if (count($rates)) {
+            foreach ($rates AS $key => $rate) {
+                $this->stdout("Update {$key}\n", Console::FG_BLUE);
+                $rateItem = Currency::findOne(['code' => $key]);
+                if ($rateItem) {
+                    $rateItem->scenario = 'update';
+                    $rateItem->rate = $rate;
+                    $rateItem->save();
+                }
             }
         }
+        $this->stdout("Finished update\n", Console::FG_GREEN);
     }
 
     /**
@@ -36,22 +40,30 @@ class CronController extends Controller {
     protected function getRates() {
         $default = Currency::find()->andWhere(['is_default' => 1])->asArray()->one();
         $currencies = Currency::find()->andWhere(['is_default' => 0])->asArray()->all();
-        $curList = [];
-        foreach ($currencies AS $currency) {
-            $curList[] = "'{$default['code']}{$currency['code']}'";
-        }
-        $params = "SELECT * FROM yahoo.finance.xchange WHERE pair IN (" . implode(', ', $curList) . ")";
-        $query = 'http://query.yahooapis.com/v1/public/yql?' . http_build_query([
-                    'format' => 'json',
-                    'q' => $params,
-                    'env' => 'store://datatables.org/alltableswithkeys',
-        ]);
-        $result = file_get_contents($query);
         $rates = [];
-        if ($result) {
-            $data = json_decode($result, true);
-            foreach ($data['query']['results']['rate'] AS $rate) {
-                $rates[substr($rate['id'],3)] = $rate['Bid'];
+        if (count($currencies)) {
+            $curList = [];
+            foreach ($currencies AS $currency) {
+                $curList[] = "'{$default['code']}{$currency['code']}'";
+            }
+            $params = "SELECT * FROM yahoo.finance.xchange WHERE pair IN (" . implode(', ', $curList) . ")";
+            $query = 'http://query.yahooapis.com/v1/public/yql?' . http_build_query([
+                        'format' => 'json',
+                        'q' => $params,
+                        'env' => 'store://datatables.org/alltableswithkeys',
+            ]);
+            $result = file_get_contents($query);
+            if ($result) {
+                $data = json_decode($result, true);
+                $ratesRes = [];
+                if (count($currencies) == 1) {
+                    $ratesRes[] = $data['query']['results']['rate'];
+                } else {
+                    $ratesRes = $data['query']['results']['rate'];
+                }
+                foreach ($ratesRes AS $rate) {
+                    $rates[substr($rate['id'], 3)] = $rate['Bid'];
+                }
             }
         }
         return $rates;
